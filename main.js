700f8559cb2335bfde72cc64ee5d6dd9faea2385
Cailean Fernandes <cailean17@gmail.com>
2022-08-08 17:19:02 -0700

    short

--- main.js
+++ main.js
@@ -217,7 +217,7 @@ const APIController = (function() {
 
     const _getTopArtists = async(token) => {
      
-        const result = await fetch('https://api.spotify.com/v1/me/top/artists?limit=7&time_range=medium_term', {
+        const result = await fetch('https://api.spotify.com/v1/me/top/artists?limit=7&time_range=short_term', {
             method:'GET',
             headers: {
                
@@ -262,7 +262,7 @@ const APIController = (function() {
     }
     const _getTopTracks = async(token) => {
      
-        const result = await fetch('https://api.spotify.com/v1/me/top/tracks?limit=3&time_range=medium_term', {
+        const result = await fetch('https://api.spotify.com/v1/me/top/tracks?limit=3&time_range=short_term', {
             method:'GET',
             headers: {
                
