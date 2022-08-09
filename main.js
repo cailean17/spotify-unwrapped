71b286be7fa3feb8cefe7e57deb5f68e1be41a34
Cailean Fernandes <cailean17@gmail.com>
2022-08-08 22:58:40 -0700

    test

--- main.js
+++ main.js
@@ -390,7 +390,7 @@ const UIController = (function() {
             <div class = "row">
             <div class = "row>
               <div class="col -xs">
-                <div class = "xop-box", onclick = "track_playback_function(token, track1analysis.uri)}" style = "background:url(${track1.album.images[0].url}) center/200px 200px no-repeat; cursor:pointer">
+                <div class = "xop-box", onclick = "track_playback_function(token, track1analysis.uri)" style = "background:url(${track1.album.images[0].url}) center/200px 200px no-repeat; cursor:pointer">
                  
                     <div class = "overlay" >
                     <i class="fa fa-play-circle-o icon" aria-hidden="true"></i>
