c82efeeaf680920e3e9f0dcffed42cefa48b2fa1
Cailean Fernandes <cailean17@gmail.com>
2022-08-08 20:41:24 -0700

    added missing class

--- main.js
+++ main.js
@@ -372,7 +372,7 @@ const UIController = (function() {
                 <div class = "xop-box", style = "background:url(${track1.album.images[0].url}) center/200px 200px no-repeat">
                  
                     <div class = "overlay">
-                    <i class="fa fa-play-circle-o" aria-hidden="true"></i>
+                    <i class="fa fa-play-circle-o icon" aria-hidden="true"></i>
 
                     </div>
                 </div>
