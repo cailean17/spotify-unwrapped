0ac505d4555195b8201751b821cf63f9e0c09526
Cailean Fernandes <cailean17@gmail.com>
2022-08-10 15:40:12 -0700

    hello

--- main.js
+++ main.js
@@ -425,10 +425,10 @@ const UIController = (function() {
             
 
         },
-        async populateTopTracksList(track1, track2, track3, track1analysis, track2analysis, track3analysis, track_playback_function, token){
-            var track1_descriptions =  await determineTrackDescriptions(track1analysis);
-            var track2_descriptions =  await determineTrackDescriptions(track2analysis);
-            var track3_descriptions =  await determineTrackDescriptions(track3analysis);
+         populateTopTracksList(track1, track2, track3, track1analysis, track2analysis, track3analysis, track_playback_function, token){
+            var track1_descriptions =   determineTrackDescriptions(track1analysis);
+            var track2_descriptions =   determineTrackDescriptions(track2analysis);
+            var track3_descriptions =   determineTrackDescriptions(track3analysis);
             var track1Mood = runNeuralNetwork(track1analysis.danceability, track1analysis.acousticness, track1analysis.energy, track1analysis.instrumentalness, track1analysis.liveness, track1analysis.valence, track1analysis.speechiness, net);
             var track2Mood = runNeuralNetwork(track2analysis.danceability, track2analysis.acousticness, track2analysis.energy, track2analysis.instrumentalness, track2analysis.liveness, track2analysis.valence, track2analysis.speechiness, net);
             var track3Mood = runNeuralNetwork(track3analysis.danceability, track3analysis.acousticness, track3analysis.energy, track3analysis.instrumentalness, track3analysis.liveness, track3analysis.valence, track3analysis.speechiness, net);
@@ -458,10 +458,10 @@ const UIController = (function() {
                  </div>
                   
                       <p class="lead" style="font-size:15px">
-                      ${track1_descriptions}
+                      ${track1_descriptions[0]}
                       </p>
                       <p class="lead" style="font-size:15px">
-                      ${track1_descriptions}
+                      ${track1_descriptions[1]}
                       </p>
              </div>
            
