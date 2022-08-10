c557d4b8bce421be7f0b3c6bf9f0352e53f0ada4
Cailean Fernandes <cailean17@gmail.com>
2022-08-10 09:26:07 -0700

    changed

--- main.js
+++ main.js
@@ -149,7 +149,8 @@ var nnData= JSON.parse(data);
             console.log("adding to description");
         }
         console.log("FINAL description" + final_description_array.toString());
-        return final_description_array;
+        var returned_desc = final_description_array;
+        return returned_desc;
         
     }
     
