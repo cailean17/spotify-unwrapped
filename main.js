4227f08e07ce39772d2896ca06bcfa0066a0eec1
Cailean Fernandes <cailean17@gmail.com>
2022-08-09 20:10:16 -0700

    added await

--- main.js
+++ main.js
@@ -424,9 +424,9 @@ const UIController = (function() {
             document.querySelector(DOMElements.topTrack1).innerHTML = track1.name;
             document.querySelector(DOMElements.topTrack2).innerHTML = track2.name;
             document.querySelector(DOMElements.topTrack3).innerHTML = track3.name;
-            var track1_descriptions = determineTrackDescriptions(track1analysis);
-            var track2_descriptions = determineTrackDescriptions(track2analysis);
-            var track3_descriptions = determineTrackDescriptions(track3analysis);
+            var track1_descriptions = await determineTrackDescriptions(track1analysis);
+            var track2_descriptions = await determineTrackDescriptions(track2analysis);
+            var track3_descriptions = await determineTrackDescriptions(track3analysis);
             document.querySelector(DOMElements.topTrack1).insertAdjacentHTML("beforeend", 
             
             ` 
@@ -449,10 +449,10 @@ const UIController = (function() {
                  </div>
                   
                       <p class="lead" style="font-size:15px">
-                      ${track1_descriptions}
+                      ${track1_descriptions[0]}
                       </p>
                       <p class="lead" style="font-size:15px">
-                      ${track1_descriptions}
+                      ${track1_descriptions[1]}
                       </p>
              </div>
            
