acad794025f77db0bd4413ada25facf796730214
Cailean Fernandes <cailean17@gmail.com>
2022-08-10 21:45:18 -0700

    whoops fixed now

--- main.js
+++ main.js
@@ -484,7 +484,7 @@ const UIController = (function() {
                 <div class = "xop-box" style = "background:url(${track1.album.images[0].url}) center/200px 200px no-repeat; cursor:pointer">
                  
                     <div class = "overlay" >
-                   
+                    <button class="btn" > <i class="fa fa-play-circle-o icon" aria-hidden="true"></i> </button>
 
                     </div>
                 </div>
@@ -501,7 +501,7 @@ const UIController = (function() {
                  <div class = "xop-box-small" style = "background:url(${recommendations.tracks[0].album.images[0].url}) center/100px 100px no-repeat; cursor:pointer">
                  
                     <div class = "overlay" >
-                   
+                    <button class="btn" > <i class="fa fa-play-circle-o icon" aria-hidden="true"></i> </button>
 
                     </div>
                 </div>
