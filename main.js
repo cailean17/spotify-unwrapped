b7371da9bddf076105a5c7a499820e3e00f6fd8e
Cailean Fernandes <cailean17@gmail.com>
2022-08-10 22:29:29 -0700

    rec padding

--- main.js
+++ main.js
@@ -498,6 +498,7 @@ const UIController = (function() {
                  <div class = "col-xs px-4 ">
                  <p class = "lead"> Recommendations </p>
                  <div class = "row justify-content-around">
+                 <div class = "col-xs pe-4">
                  <div class = "xop-box-small" style = "background:url(${recommendations.tracks[0].album.images[0].url}) center/100px 100px no-repeat; cursor:pointer">
                  
                     <div class = "overlay" >
@@ -505,19 +506,24 @@ const UIController = (function() {
 
                     </div>
                 </div>
-                <div class = "xop-box-small px-4" style = "background:url(${recommendations.tracks[1].album.images[0].url}) center/100px 100px no-repeat; cursor:pointer">
+                </div>
+                <div class = "col-xs px-4 ">
+                <div class = "xop-box-small" style = "background:url(${recommendations.tracks[1].album.images[0].url}) center/100px 100px no-repeat; cursor:pointer">
                  
-                <div class = "overlay" >
-                <button class="btn" > <i class="fa fa-play-circle-o icon" aria-hidden="true"></i> </button>
+                    <div class = "overlay" >
+                    <button class="btn" > <i class="fa fa-play-circle-o icon" aria-hidden="true"></i> </button>
 
                     </div>
                 </div>
+                </div>
+                <div class = "col-xs ps-4 ">
                 <div class = "xop-box-small" style = "background:url(${recommendations.tracks[2].album.images[0].url}) center/100px 100px no-repeat; cursor:pointer">
                  
                     <div class = "overlay" >
                     <button class="btn" > <i class="fa fa-play-circle-o icon" aria-hidden="true"></i> </button>
 
                     </div>
+                </div>
                 </div>
                 </div>
                 </div>
