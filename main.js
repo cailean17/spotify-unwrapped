c1cbe16cc65e4257d7ad3eee995d30ed1f651708
Cailean Fernandes <cailean17@gmail.com>
2022-08-10 19:52:49 -0700

    this works maybe

--- main.js
+++ main.js
@@ -459,7 +459,7 @@ const UIController = (function() {
             <div class = "container my-2">
             <div class = "row">
             
-            <div class = "row>
+            <div class = "row gx-5>
               <div class="col -xs">
                 <div class = "xop-box" style = "background:url(${track1.album.images[0].url}) center/200px 200px no-repeat; cursor:pointer">
                  
@@ -475,7 +475,7 @@ const UIController = (function() {
                     <h1 class = "rating-t"> ${track1Mood} </h1>
 
                  </div>
-                 <div class = "col-xs ps-5">
+                 <div class = "col-xs ">
                  <p class = "lead"> Recommendations </p>
                  <div class = "row justify-content-around">
                  <p> 1 </p>
