7ee2d13838d82716e1a2c1bc0335bf345493f070
Cailean Fernandes <cailean17@gmail.com>
2022-08-10 22:25:51 -0700

    does this work

--- main.js
+++ main.js
@@ -505,7 +505,7 @@ const UIController = (function() {
 
                     </div>
                 </div>
-                <div class = "xop-box-small" style = "background:url(${recommendations.tracks[1].album.images[0].url}) center/100px 100px no-repeat; cursor:pointer">
+                <div class = "xop-box-small px-4" style = "background:url(${recommendations.tracks[1].album.images[0].url}) center/100px 100px no-repeat; cursor:pointer">
                  
                 <div class = "overlay" >
                 <button class="btn" > <i class="fa fa-play-circle-o icon" aria-hidden="true"></i> </button>
