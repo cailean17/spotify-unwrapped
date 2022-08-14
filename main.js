481dacc2ba75d8c7f2f5ed0605c368c002ea32a1
Cailean Fernandes <cailean17@gmail.com>
2022-08-13 20:36:52 -0700

    fire

--- main.js
+++ main.js
@@ -950,7 +950,7 @@ const UIController = (function() {
                 <p class = "lead" style = "font-size:10px">${recommendations.tracks[1].artists[0].name}</p>
                 </div>
                 <div class = "col-xs ps-4 ">
-                <div class = "xop-box-small" id = "track1_recommendation3album"  style = "background:url(${recommendations.tracks[0].album.images[0].url}) center/100px 100px no-repeat; cursor:pointer">
+                <div class = "xop-box-small" id = "track1_recommendation3album"  style = "background:url(${recommendations.tracks[2].album.images[0].url}) center/100px 100px no-repeat; cursor:pointer">
                  
                     <div class = "overlay" >
                     <button class="btn" > <i class="fa fa-play-circle-o icon" aria-hidden="true"></i> </button>
