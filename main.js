3085fb519a839e6c555e2174ced64b07af28387f
Cailean Fernandes <cailean17@gmail.com>
2022-12-05 01:02:50 -0800

    bruh

--- main.js
+++ main.js
@@ -1011,7 +1011,7 @@ const UIController = (function() {
                 labels: labels,
                 datasets: [{
                 label: 'Nicheness',
-                fill: true,
+               
                 borderColor: 'rgba(75, 192, 192, 1)',
                 backgroundColor: 'rgba(75, 192, 192, 0.2)',
                 color : "#FFF",
