0407957ac955ecb45e82e8c2c68ac0b2417fedcc
Cailean Fernandes <cailean17@gmail.com>
2022-12-05 17:41:53 -0800

    forgot margin variable

--- main.js
+++ main.js
@@ -265,6 +265,8 @@ var testingData = JSON.parse(testing_data);
  }
 
  function confusionMatrix(options) {
+        var margin = {top: 50, right: 50, bottom: 100, left: 100};
+
 	    var width = 250,
 	    height = 250,
 	    data = options.data,
