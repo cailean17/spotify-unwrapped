0103c19c9d574436f1b8a7959ae83532dc81d0c7
Cailean Fernandes <55571023+cailean17@users.noreply.github.com>
2023-10-20 16:09:18 -0700

    reducing number of hidden layers

--- main.js
+++ main.js
@@ -49,7 +49,7 @@ var testingData = JSON.parse(testing_data);
         output:output[i],
         });
     }
-    const net = new brain.NeuralNetwork({hiddenLayers:[5,6], activation: 'relu'});
+    const net = new brain.NeuralNetwork({hiddenLayers:[4]});
 
     const stats = net.train(trainingData, {
         
