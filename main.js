33f87c48d89e434ecf83edf1b53f3b22bea2a098
Cailean Fernandes <55571023+cailean17@users.noreply.github.com>
2023-10-20 16:26:58 -0700

    increasing neurons

--- main.js
+++ main.js
@@ -49,7 +49,7 @@ var testingData = JSON.parse(testing_data);
         output:output[i],
         });
     }
-    const net = new brain.NeuralNetwork({hiddenLayers:[22]});
+    const net = new brain.NeuralNetwork({hiddenLayers:[22,20]});
 
     const stats = net.train(trainingData, {
         
