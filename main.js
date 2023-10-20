f1888dcf9465cb6a8e421084ef4e91dceec93725
Cailean Fernandes <55571023+cailean17@users.noreply.github.com>
2023-10-20 16:15:22 -0700

    refactoring hidden layers

--- main.js
+++ main.js
@@ -49,7 +49,7 @@ var testingData = JSON.parse(testing_data);
         output:output[i],
         });
     }
-    const net = new brain.NeuralNetwork({hiddenLayers:[4]});
+    const net = new brain.NeuralNetwork({hiddenLayers:[14]});
 
     const stats = net.train(trainingData, {
         
