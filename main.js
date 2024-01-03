2a19bcd467f0ba620655866b412070cc2057ba51
Cailean Fernandes <55571023+cailean17@users.noreply.github.com>
2024-01-02 19:03:13 -0800

    testing new modals

--- main.js
+++ main.js
@@ -934,13 +934,16 @@ const UIController = (function() {
                     <div class="modal-dialog" role="document">
                         <div class="modal-content">
                         <div class="modal-header">
-                            <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                            <h5 class="modal-title" id="exampleModalLabel">How'd you like this song?</h5>
                             <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                            <span aria-hidden="true">&times;</span>
+
                             </button>
                         </div>
                         <div class="modal-body">
-                            "
+                            <div class = "row-xs"
+                                <i class="bi bi-hand-thumbs-up"></i>
+                                <i class="bi bi-hand-thumbs-down"></i>
+                            </div>
                         </div>
                         <div class="modal-footer">
                             <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
