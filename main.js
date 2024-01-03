5a444ba7c9638c3201c92c74877b2921988219fa
Cailean Fernandes <55571023+cailean17@users.noreply.github.com>
2024-01-02 18:25:10 -0800

    fixed id error

--- main.js
+++ main.js
@@ -930,7 +930,7 @@ const UIController = (function() {
 
                     </div>
 
-                    <div class="modal fade" id="#recModal_${recommendations.tracks[1].id}" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                    <div class="modal fade" id="recModal_${recommendations.tracks[1].id}" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
                     <div class="modal-dialog" role="document">
                         <div class="modal-content">
                         <div class="modal-header">
